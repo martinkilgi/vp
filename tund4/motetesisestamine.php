bf942293ee031437cd181079c46ee066243d4435
Martin Kilgi <martinkilgi@users.noreply.github.com>
2020-09-26 19:14:26 +0300

    neljanda tunni kodutöö

---
+++ tund4/motetesisestamine.php
@@ -0,0 +1,62 @@
+<?php
+
+$username = "Martin Kilgi";
+
+//var_dump($_POST);
+require("../../../config.php");
+$database = "if20_martin_kl_2";
+if(isset($_POST["ideasubmit"]) and !empty($_POST["ideainput"])) {
+    //loome andmebaasiga ühenduse
+    $conn = new mysqli($serverhost, $serverusername, $serverpassword, $database);
+    //valmistan ette SQL käsu andmete kirjutamiseks
+    $stmt = $conn->prepare("INSERT INTO myideas (idea) VALUES(?)");
+    echo $conn->error;
+    //i - integer, d - decimal ehk murdarv, s - string
+    $stmt->bind_param("s", $_POST["ideainput"]);
+    $stmt->execute();
+    $stmt->close();
+    $conn->close();
+}
+
+//loen andmebaasist senised mõtted
+$ideahtml = "";
+$conn = new mysqli($serverhost, $serverusername, $serverpassword, $database);
+$stmt = $conn->prepare("SELECT idea FROM myideas");
+//seon tulemuse muutujaga
+$stmt->bind_result($ideafromdb);
+$stmt->execute();
+while ($stmt->fetch()) {
+    $ideahtml .= "<p>" .$ideafromdb ."</p>";
+}
+
+$stmt->close();
+$conn->close();
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="et">
+<head>
+  <meta charset="utf-8">
+  <title><?php echo $username; ?> progeb veebi</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<img src="../img/vp_banner.png" alt="Veebiprogrammeerimise kursuse logo">
+  <h1><?php echo $username; ?></h1>
+  <p id="esimene">See veebileht on loodud õppetöö käigus ning ei sisalda mingit tõsiseltvõetavat sisu!</p>
+  <p id="teine">Leht on loodud veebiprogrammeerimise raames <a href="https://www.tlu.ee" target="_blank">Tallinna Ülikooli</a> Digitehnoloogiate instituudis.</p>
+  <p id="kolmas">Siit saad minna otse <a href="https://www.tlu.ee/infotehnoloogiaosakond" target="_blank">TLÜ infotehnoloogiaosakonda.</a></p>
+<h1>Siin saad kirja panna enda mõtted!</h1>
+<hr>
+  <form method="POST">
+    <label>Kirjutage oma esimene pähe tulev mõte!</label>
+    <input type="text" name="ideainput" placeholder="mõttekoht">
+    <input type="submit" name="ideasubmit" value="Saada mõte teele!">
+  </form>
+  <hr>
+  <a href="home.php">Siit pääsed tagasi avalehele</a><br />
+  <a href="motetelist.php">Siit saad oma kirja pandud mõtteid vaatama minna</a>
+</body>
+</html>
---
+++ tund4/userreg.php
@@ -0,0 +1,89 @@
+<?php
+
+$username = "Martin Kilgi";
+
+//Errorite muutujad
+$inputerror = "";
+$firstnameerror = "";
+$lastnameerror = "";
+$gendererror = "";
+$emailerror = "";
+$passworderror = "";
+
+//info muutujad
+$firstname = "";
+$lastname = "";
+$genderinput = "";
+$emailinfo = "";
+$passwordinput = "";
+$passwordsecondaryinput = "";
+
+//kas vajutati salvestusnuppu
+if (isset($_POST["regsubmit"])) {
+  $firstname = ($_POST["firstnameinput"]);
+  $lastname = ($_POST["lastnameinput"]);
+  $genderinput = ($_POST["genderinput"]);
+  $emailinfo = ($_POST["emailinput"]);
+
+  if(strlen($_POST["passwordinput"]) < 8) {
+    $passworderror = "Salasõna on liiga lühike.";
+} if (($_POST["passwordinput"]) !== ($_POST["passwordsecondaryinput"])) {
+    $passworderror = "Teine salasõna ei kattu esimesega";
+}
+  if(empty($_POST["firstnameinput"]) or empty($_POST["genderinput"]) or empty($_POST["lastnameinput"]) or empty($_POST["emailinput"]) or empty($_POST["passwordinput"]) or empty($_POST["passwordsecondaryinput"])) { 
+    $inputerror .= "Osa infot on sisestamata!";
+  }
+  if (empty($inputerror) && empty($firstnameerror) && empty($lastnameerror) && empty($gendererror) && empty($emailerror) && empty($passworderror)) {
+
+  
+}
+
+
+}
+
+
+require("header.php");
+?>
+
+<img src="../img/vp_banner.png" alt="Veebiprogrammeerimise kursuse logo">
+  <h1><?php echo $username; ?></h1>
+  <p id="esimene">See veebileht on loodud õppetöö käigus ning ei sisalda mingit tõsiseltvõetavat sisu!</p>
+  <p id="teine">Leht on loodud veebiprogrammeerimise raames <a href="https://www.tlu.ee" target="_blank">Tallinna Ülikooli</a> Digitehnoloogiate instituudis.</p>
+  <p id="kolmas">Siit saad minna otse <a href="https://www.tlu.ee/infotehnoloogiaosakond" target="_blank">TLÜ infotehnoloogiaosakonda.</a></p>
+  <li><a href="home.php">Siit saab minna tagasi avalehele</a><br /></li>
+<hr>
+<form method="POST">
+
+<label for="firstname">Eesnimi</label>
+<input type="text" name="firstnameinput" id="firstnameinput" value="<?php echo $firstname; ?>"><span><?php echo $firstnameerror; ?></span>
+<br>
+<label for="lastname">Perekonnanimi</label>
+<input type="text" name="lastnameinput" id="lastnameinput" value="<?php echo $lastname; ?>"><span><?php echo $lastnameerror; ?></span>
+<br>
+<label for="gendermale">Mees</label>
+<input type="radio" name="genderinput" id="gendermaleinput" value="1" <?php if($genderinput == "1"){echo " checked";}?>><span><?php echo $gendererror; ?></span>
+<br>
+<label for="genderfemale">Naine</label>
+<input type="radio" name="genderinput" id="genderfemaleinput" value="2" <?php if($genderinput == "2"){echo " checked";}?>><span><?php echo $gendererror; ?></span>
+<br>
+<label for="email">E-mail</label>
+<input type="email" name="emailinput" id="emailinput" value="<?php echo $emailinfo; ?>"><span><?php echo $emailerror; ?></span>
+<br>
+<label for="password">Salasõna</label>
+<input type="password" name="passwordinput" id="passwordinput"><span><?php echo $passworderror; ?></span>
+<br>
+<label for="passwordsecondary">Salasõna teist korda</label>
+<input type="password" name="passwordsecondaryinput" id="passwordsecondaryinput"><span><?php echo $passworderror; ?></span>
+<br>
+<input type="submit" name="regsubmit" value="Registreeru">
+
+</form>
+<p><?php echo $inputerror; ?></p>
+</body>
+</html>
+
+
+
+
+
+
